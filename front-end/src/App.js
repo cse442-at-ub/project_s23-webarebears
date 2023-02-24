4a463e6b8d4d4d8f7a332c08a51d11d29f9f994e
lyao <lyao3@buffalo.edu>
2023-02-23 19:53:52 -0500

    frontend

---
+++ front-end/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import './App.css';
+import Register from './pages/register';
+import Home from './pages/home';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/register' element={<Register/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
