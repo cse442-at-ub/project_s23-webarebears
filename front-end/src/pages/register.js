3e7650e8c14acaa1d9c1e036162d25dd2499e736
lyao <lyao3@buffalo.edu>
2023-02-24 01:33:14 -0500

    register page WIP

--- front-end/src/pages/register.js
+++ front-end/src/pages/register.js
@@ -1,8 +1,31 @@
 import React from "react";
+import './register.css'
 
 const Register = () => {
     return(
-        <div>Register</div>
+        <div className="page">
+            <div className="register-title">
+                <div>SIGN UP WITH YOUR</div>
+                <div>USERNAME</div>
+            </div>
+            
+            <div className="form">
+              <div className="form-body">
+                  <div className="username">
+                      <input  type="username" id="username" className="form__input" placeholder="Enter Your Username"/>
+                  </div>
+                  <div className="password">
+                      <input className="form__input" type="password"  id="password" placeholder="Enter Your Password"/>
+                  </div>
+                  <div className="confirm-password">
+                      <input className="form__input" type="password" id="confirmPassword" placeholder="Re-enter Password"/>
+                  </div>
+              </div>
+              <div class="footer">
+                  <button type="submit" class="btn">Register</button>
+              </div>
+          </div>      
+        </div>
     );
 }
 
