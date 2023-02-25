46ef0bf487af60f63ce1658c20e7632c7037083a
junstinr <notjun01@gmail.com>
2023-02-24 19:22:56 -0500

    created login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Routes, Route}
 import './App.css';
 import Register from './pages/register';
 import Home from './pages/home';
-
+import Login from './pages/login'
 
 function App() {
   return (
     <Router>
       <Routes>
+
         <Route exact path='/' element={<Home/>} />
         <Route path='/register' element={<Register/>} />
+        <Route path = "/login" element = {<Login/>} />
+        
       </Routes>
     </Router>
   );
