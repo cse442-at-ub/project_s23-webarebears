74f13813e24f1c1481e0b9b8fd75203b35fc5dfd
cameronjtoy <cameron.j.toy@gmail.com>
2023-04-24 11:35:54 -0400

    Bug Fixes for Changing Password #61

---
+++ backend/search.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['username'])) {
+        echo json_encode(['error' => 'Not authenticated']);
+        exit();
+    }
+
+    require('server.php');
+
+    $searchQuery = $_GET['q'] ?? '';
+
+    if (trim($searchQuery) === '') {
+        echo json_encode(['error' => 'Empty search query']);
+        exit();
+    }
+
+    $searchQuery = mysqli_real_escape_string($db_connection, $searchQuery);
+
+    $query = "
+        (SELECT description as result, 'User_debts' as type FROM `User_debts` WHERE description LIKE '%$searchQuery%')
+        UNION
+        (SELECT description as result, 'Tasks' as type FROM `Tasks` WHERE description LIKE '%$searchQuery%')
+        UNION
+        (SELECT username as result, 'User Accounts' as type FROM `User Accounts` WHERE username LIKE '%$searchQuery%')
+    ";
+
+    $result = mysqli_query($db_connection, $query);
+
+    if ($result) {
+        $results = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $results[] = $row;
+        }
+        echo json_encode(['results' => $results]);
+    } else {
+        echo json_encode(['error' => 'Error executing search query']);
+    }
+?>
